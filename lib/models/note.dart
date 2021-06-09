8b78b9bf87b4167d958e8ad8dd7aee142c33ac3f
ritheshjarvis <ritheshjarvis@gmail.com>
2021-06-09 08:56:39 +0530

    Model Class created for Databse

---
+++ lib/models/note.dart
@@ -0,0 +1,63 @@
+import 'dart:html';
+
+class Note {
+  int _id;
+  String _title;
+  String _description;
+  String _date;
+  int _priority;
+
+  Note(this._title, this._date, this._priority, [this._description]);
+
+  // Named Constructor
+  Note.withID(this._id, this._title, this._date, this._priority,
+      [this._description]);
+
+  int get id => id;
+
+  int get priority => _priority;
+
+  String get title => _title;
+
+  String get description => _description;
+
+  String get date => _date;
+
+  set title(String newTitle) {
+    if (newTitle.length <= 255) {
+      this._title = newTitle;
+    }
+  }
+
+  set priority(int newPriority) {
+    if (newPriority >= 1 && newPriority <= 2) {
+      this._priority = newPriority;
+    }
+  }
+
+  set date(String newDate) {
+    this._date = newDate;
+  }
+
+  // Convert Note object -> Map pbject
+  Map<String, dynamic> toMap() {
+    var map = Map<String, dynamic>();
+    if (id != null) {
+      map['id'] = _id;
+    }
+    map['title'] = _title;
+    map['description'] = _description;
+    map['date'] = _date;
+    map['priority'] = _priority;
+    return map;
+  }
+
+//Convert Map object -> Note object
+  Note.fromMapObject(Map<String, dynamic> map) {
+    this._id = map['id'];
+    this._title = map['title'];
+    this._description = map['description'];
+    this._priority = map['priority'];
+    this._date = map['date'];
+  }
+}
